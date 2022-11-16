8bcc98f2c8dfd8e4fe2b8f0e45718c84636a7996
Luis Angel <lang@zuntrix.com>
2022-11-15 22:40:19 -0500

    162. Layout y Páginas adicionales

---
+++ pages/theme-changer.tsx
@@ -0,0 +1,9 @@
+import { MainLayout } from "../components/layout";
+
+export default function ThemeChangerPage() {
+    return (
+        <MainLayout>
+            <h1>Cambiar tema</h1>
+        </MainLayout>
+    )
+  }
